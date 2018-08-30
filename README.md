ef0b115215d1cea55de1b994608f9bb309ee8cd0
2mrmikeliu <server_mike@outlook.com>
2018-08-30 21:56:10 +0800

    上传矩阵库 书写手册README.md

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-"# matrix-library-on-CPP" 
+# matrix-library-on-CPP version 0.1
+
+# 手册
+1. 类
+	1. matrix类
+		成员
+		
+		函数void PrintFormat()	以矩阵的标准形式输出
+		
+		函数void PutArray(type **Array,int Row,int Column)	将Row行Column列的数组Array转化为矩阵，并且存放在矩阵中
+		
+		函数int size()	求得矩阵的大小
+		
+		函数void PutElement(type Data,int Row,int Column)	将Row行Column列的数变为Data
+		
+		函数type GetElement(int Row,int Column)	得到Row行Column列的数据
+		
+		函数type GetRowLength()	得到矩阵的行数
+		
+		函数type GetColumnLength(void)	得到矩阵的列数
+		
+		析构函数matrix(int row,int column)	定义矩阵大小是Row行Column列
+	2.函数matrix<int> operator+ (matrix<int> A,matrix<int> B)	将矩阵A和矩阵B相加（如不是同型矩阵，会抛出异常0x3f）
---
+++ lib/Matrix.h
@@ -0,0 +1,83 @@
+
+#ifndef _GLIBCXX_VECTOR
+#include<vector>
+#endif
+#ifndef _LIBLHY_MATRIX_
+	#define _LIBLHY_MATRIX_ 1
+	template<class type>
+	class matrix
+	{
+		private:
+			int X;
+			int Y;
+			vector< vector< type > > mat;
+		public:
+			void PrintFormat()
+			{
+				#ifndef _GLIBCXX_IOSTREAM
+					#include<iostream>
+				#endif
+				for(int i=0;i<X;i++)
+				{
+					for(int j=0;j<Y;j++)
+						j!=Y?std::cout<<mat[i][j]<<" ":std::cout<<mat[i][j];
+					std::cout<<std::endl;
+				}
+			}
+			void PutArray(type **Array,int Row,int Column)
+			{
+				for(int i=0;i<Row;i++)
+					for(int j=0;j<Column;j++)
+					{
+						mat[i][j]=*((int*)Array + Column*i + j);
+//						mat[i][j]=arr[i][j];
+					}
+				
+			}
+			int size(void)
+			{
+				return this->X *this->Y;
+			}
+			void PutElement(type Data,int Row,int Column)
+			{
+				mat[Row][Column]=Data;
+			}
+			type GetElement(int Row,int Column)
+			{
+				return mat[Row][Column];
+			}
+			type GetRowLength(void)
+			{return X;}
+			type GetColumnLength(void)
+			{return Y;}
+			matrix(int row,int column)
+			{
+				mat.resize(row); 
+				for(int i=0;i<row;i++)
+         			mat[i].resize(column);
+         		X=row;
+         		Y=column;
+				
+			}
+//			type** getNomal
+	};
+#endif
+matrix<int> operator+ (matrix<int> A,matrix<int> B)
+{
+	matrix<int> C(A.GetRowLength(),A.GetColumnLength());
+	if(A.GetColumnLength()==B.GetColumnLength()&&A.GetRowLength()==B.GetRowLength())
+	{
+		for(int i=0;i<A.GetColumnLength() ;i++)
+			for(int j=0;j<A.GetColumnLength() ;j++)
+				{
+					C.PutElement(A.GetElement(i,j)+B.GetElement(i,j),i,j); 
+				}
+		return C;
+	}
+	else
+	{
+		std::cout<<std::endl<<"Error : The matrix being manipulated is not a matrix of the same type."<<std::endl<<std::endl;
+		throw 0x3f;
+	}
+}
+
