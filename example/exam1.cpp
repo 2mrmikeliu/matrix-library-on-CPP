60ed3810185bc416ba6c08e28d58e6112b8b3621
2mrmikeliu <server_mike@outlook.com>
2018-08-31 09:50:59 +0800

    添加示例

---
+++ example/exam1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+#include "Matrix.h"
+matrix<int> m1(2,2),m2(2,2);
+int main()
+{
+	int arr[2][2]={{1,2},{1,3}};
+	m1.PutArray((int**)arr,2,2);
+	m2.PutArray((int**)arr,2,2);
+	(m1+m2).PrintFormat();
+	return 0;
+}
+
