ac15a012adb296f1eb912bef3b0c147ae1f894f9
2mrmikeliu <server_mike@outlook.com>
2018-08-31 09:37:53 +0800

    重载不同类型的+标识符并格式化代码

--- lib/Matrix.h
+++ lib/Matrix.h
@@ -1,83 +1,195 @@
-
 #ifndef _GLIBCXX_VECTOR
 #include<vector>
 #endif
 #ifndef _LIBLHY_MATRIX_
-	#define _LIBLHY_MATRIX_ 1
-	template<class type>
-	class matrix
-	{
-		private:
-			int X;
-			int Y;
-			vector< vector< type > > mat;
-		public:
-			void PrintFormat()
-			{
-				#ifndef _GLIBCXX_IOSTREAM
-					#include<iostream>
-				#endif
-				for(int i=0;i<X;i++)
+#define _LIBLHY_MATRIX_ 1
+template<class type>
+class matrix
+{
+	private:
+		int X;
+		int Y;
+		vector< vector< type > > mat;
+	public:
+		void PrintFormat()
+		{
+		#ifndef _GLIBCXX_IOSTREAM
+			#include<iostream>
+		#endif
+			for(int i=0; i<X; i++)
 				{
-					for(int j=0;j<Y;j++)
+					for(int j=0; j<Y; j++)
 						j!=Y?std::cout<<mat[i][j]<<" ":std::cout<<mat[i][j];
 					std::cout<<std::endl;
 				}
-			}
-			void PutArray(type **Array,int Row,int Column)
-			{
-				for(int i=0;i<Row;i++)
-					for(int j=0;j<Column;j++)
+		}
+		void PutArray(type **Array,int Row,int Column)
+		{
+			for(int i=0; i<Row; i++)
+				for(int j=0; j<Column; j++)
 					{
 						mat[i][j]=*((int*)Array + Column*i + j);
 //						mat[i][j]=arr[i][j];
 					}
-				
-			}
-			int size(void)
-			{
-				return this->X *this->Y;
-			}
-			void PutElement(type Data,int Row,int Column)
-			{
-				mat[Row][Column]=Data;
-			}
-			type GetElement(int Row,int Column)
-			{
-				return mat[Row][Column];
-			}
-			type GetRowLength(void)
-			{return X;}
-			type GetColumnLength(void)
-			{return Y;}
-			matrix(int Row,int Column)
-			{
-				mat.resize(Row); 
-				for(int i=0;i<row;i++)
-         			mat[i].resize(Column);
-         		X=Row;
-         		Y=Column;
-				
-			}
+
+		}
+		int size(void)
+		{
+			return this->X *this->Y;
+		}
+		void PutElement(type Data,int Row,int Column)
+		{
+			mat[Row][Column]=Data;
+		}
+		type GetElement(int Row,int Column)
+		{
+			return mat[Row][Column];
+		}
+		type GetRowLength(void)
+		{
+			return X;
+		}
+		type GetColumnLength(void)
+		{
+			return Y;
+		}
+		matrix(int Row,int Column)
+		{
+			mat.resize(Row);
+			for(int i=0; i<row; i++)
+				mat[i].resize(Column);
+			X=Row;
+			Y=Column;
+
+		}
 //			type** getNomal
-	};
-#endif
+};
 matrix<int> operator+ (matrix<int> A,matrix<int> B)
 {
 	matrix<int> C(A.GetRowLength(),A.GetColumnLength());
 	if(A.GetColumnLength()==B.GetColumnLength()&&A.GetRowLength()==B.GetRowLength())
-	{
-		for(int i=0;i<A.GetColumnLength() ;i++)
-			for(int j=0;j<A.GetColumnLength() ;j++)
-				{
-					C.PutElement(A.GetElement(i,j)+B.GetElement(i,j),i,j); 
-				}
-		return C;
-	}
+		{
+			for(int i=0; i<A.GetColumnLength() ; i++)
+				for(int j=0; j<A.GetColumnLength() ; j++)
+					{
+						C.PutElement(A.GetElement(i,j)+B.GetElement(i,j),i,j);
+					}
+			return C;
+		}
 	else
-	{
-		std::cout<<std::endl<<"Error : The matrix being manipulated is not a matrix of the same type."<<std::endl<<std::endl;
-		throw 0x3f;
-	}
+		{
+			std::cout<<std::endl<<"Error : The matrix being manipulated is not a matrix of the same type."<<std::endl<<std::endl;
+			throw 0x3f;
+		}
+}
+matrix<long> operator+ (matrix<long> A,matrix<long> B)
+{
+	matrix<long> C(A.GetRowLength(),A.GetColumnLength());
+	if(A.GetColumnLength()==B.GetColumnLength()&&A.GetRowLength()==B.GetRowLength())
+		{
+			for(int i=0; i<A.GetColumnLength() ; i++)
+				for(int j=0; j<A.GetColumnLength() ; j++)
+					{
+						C.PutElement(A.GetElement(i,j)+B.GetElement(i,j),i,j);
+					}
+			return C;
+		}
+	else
+		{
+			std::cout<<std::endl<<"Error : The matrix being manipulated is not a matrix of the same type."<<std::endl<<std::endl;
+			throw 0x3f;
+		}
+}
+matrix<long long> operator+ (matrix<long> A,matrix<long> B)
+{
+	matrix<int> C(A.GetRowLength(),A.GetColumnLength());
+	if(A.GetColumnLength()==B.GetColumnLength()&&A.GetRowLength()==B.GetRowLength())
+		{
+			for(int i=0; i<A.GetColumnLength() ; i++)
+				for(int j=0; j<A.GetColumnLength() ; j++)
+					{
+						C.PutElement(A.GetElement(i,j)+B.GetElement(i,j),i,j);
+					}
+			return C;
+		}
+	else
+		{
+			std::cout<<std::endl<<"Error : The matrix being manipulated is not a matrix of the same type."<<std::endl<<std::endl;
+			throw 0x3f;
+		}
+}
+matrix<unsigned int> operator+ (matrix<unsigned int> A,matrix<unsigned int> B)
+{
+	matrix<unsigned int> C(A.GetRowLength(),A.GetColumnLength());
+	if(A.GetColumnLength()==B.GetColumnLength()&&A.GetRowLength()==B.GetRowLength())
+		{
+			for(int i=0; i<A.GetColumnLength() ; i++)
+				for(int j=0; j<A.GetColumnLength() ; j++)
+					{
+						C.PutElement(A.GetElement(i,j)+B.GetElement(i,j),i,j);
+					}
+			return C;
+		}
+	else
+		{
+			std::cout<<std::endl<<"Error : The matrix being manipulated is not a matrix of the same type."<<std::endl<<std::endl;
+			throw 0x3f;
+		}
+}
+matrix<unsigned long long> operator+ (matrix<unsigned long long> A,matrix<unsigned long long> B)
+{
+	matrix<unsigned long long> C(A.GetRowLength(),A.GetColumnLength());
+	if(A.GetColumnLength()==B.GetColumnLength()&&A.GetRowLength()==B.GetRowLength())
+		{
+			for(int i=0; i<A.GetColumnLength() ; i++)
+				for(int j=0; j<A.GetColumnLength() ; j++)
+					{
+						C.PutElement(A.GetElement(i,j)+B.GetElement(i,j),i,j);
+					}
+			return C;
+		}
+	else
+		{
+			std::cout<<std::endl<<"Error : The matrix being manipulated is not a matrix of the same type."<<std::endl<<std::endl;
+			throw 0x3f;
+		}
 }
 
+matrix<float> operator+ (matrix<float> A,matrix<float> B)
+{
+	matrix<float> C(A.GetRowLength(),A.GetColumnLength());
+	if(A.GetColumnLength()==B.GetColumnLength()&&A.GetRowLength()==B.GetRowLength())
+		{
+			for(int i=0; i<A.GetColumnLength() ; i++)
+				for(int j=0; j<A.GetColumnLength() ; j++)
+					{
+						C.PutElement(A.GetElement(i,j)+B.GetElement(i,j),i,j);
+					}
+			return C;
+		}
+	else
+		{
+			std::cout<<std::endl<<"Error : The matrix being manipulated is not a matrix of the same type."<<std::endl<<std::endl;
+			throw 0x3f;
+		}
+}
+
+matrix<double> operator+ (matrix<int> A,matrix<int> B)
+{
+	matrix<int> C(A.GetRowLength(),A.GetColumnLength());
+	if(A.GetColumnLength()==B.GetColumnLength()&&A.GetRowLength()==B.GetRowLength())
+		{
+			for(int i=0; i<A.GetColumnLength() ; i++)
+				for(int j=0; j<A.GetColumnLength() ; j++)
+					{
+						C.PutElement(A.GetElement(i,j)+B.GetElement(i,j),i,j);
+					}
+			return C;
+		}
+	else
+		{
+			std::cout<<std::endl<<"Error : The matrix being manipulated is not a matrix of the same type."<<std::endl<<std::endl;
+			throw 0x3f;
+		}
+}
+#endif
